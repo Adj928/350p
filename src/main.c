0386929bccda9d18b8f6d111a4c3f8ccffdeb2d6
Oliver Kennedy <okennedy@buffalo.edu>
2025-08-20 21:11:34 -0400

    Filling out the test repl

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
  * @author Oliver Kennedy
  */
 #include "buffer.h"
+#include "conventions.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -16,6 +17,8 @@ int main(int argc, char **argv) {
   BufferManager manager;
   uint8_t initialized = 0;
   char cmd;
+  unsigned int register_count = 0;
+  Frame **registers;
 
   while (1) {
     int ret = scanf("%c", &cmd);
@@ -31,12 +34,60 @@ int main(int argc, char **argv) {
     case 'i': {
       unsigned int frame_count = 0;
       CHECK_ERROR(initialized, "Buffer manager already initialized");
-      ret = scanf("%u", &frame_count);
+
+      ret = scanf("%u %u", &frame_count, &register_count);
       CHECK_ERROR(ret < 0, "Error parsing frame count");
+
       ret = buffer_init(&manager, db_file, frame_count);
-      CHECK_ERROR(ret < 0, "Error initializing buffer manager");
+      CHECK_ERROR(ret, "Error initializing buffer manager");
+
+      registers = calloc(register_count, sizeof(Frame *));
+      CHECK_ERROR(registers != NULL, "Error initializing test registers");
+
       initialized = 1;
       printf("Buffer manager initialized\n");
+    } break;
+
+    case 'p': {
+      unsigned int target_register = 0;
+      PageID target_page = 0;
+
+      ret = scanf("%u %lu", &target_register, &target_page);
+      CHECK_ERROR(ret < 0, "Error parsing frame count");
+
+      ret = buffer_pin(&manager, target_page, &registers[target_register]);
+      CHECK_ERROR(ret, "Error pinning page");
+
+      printf("Page %lu pinned to register %u\n", target_page, target_register);
+    }
+
+    case 'w': {
+      unsigned int target_register = 0;
+
+      ret = scanf("%u", &target_register);
+      CHECK_ERROR(ret < 0, "Error parsing target_register");
+      CHECK_ERROR(registers[target_register] == NULL,
+                  "Target register is not in use");
+
+      for (uint64_t len_read = 0; len_read < PAGE_SIZE;) {
+        ret =
+            fread(*registers[target_register], 1, PAGE_SIZE - len_read, stdin);
+        CHECK_ERROR(ret <= 0, "Error reading write data");
+        len_read += ret;
+      }
+    } break;
+
+    case 'r': {
+      unsigned int target_register = 0;
+
+      ret = scanf("%u", &target_register);
+      CHECK_ERROR(ret < 0, "Error parsing target_register");
+      CHECK_ERROR(registers[target_register] == NULL,
+                  "Target register is not in use");
+
+      for (uint64_t i = 0; i < PAGE_SIZE; i++) {
+        putc((*registers[target_register])[i], stdout);
+      }
     } break;
     }
   }
