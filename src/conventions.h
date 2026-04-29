c0dbfcf8bf25daed91ae9acd38939bd1459ac44e
Adj928 <98960310+Adj928@users.noreply.github.com>
2026-04-29 12:37:27 -0400

    Update conventions.h

--- src/conventions.h
+++ src/conventions.h
@@ -5,8 +5,8 @@
 #ifndef CONVENTIONS_H_SHIELD
 #define CONVENTIONS_H_SHIELD
 
+#include <stddef.h>
 #include <stdint.h>
-#include <errno.h>
 
 /// The result of a function call.  Note that since SUCCESS = 0 and ERROR = 1 you can use the following code construct:
 ///
@@ -32,4 +32,19 @@ typedef uint64_t PageID;
 /// A standard Frame
 typedef uint8_t Frame[PAGE_SIZE];
 
+/// The type of a data value or key
+typedef enum DataType {
+  STRING = 1,
+  INT_64 = 2,
+  FLOAT_64 = 3,
+} DataType;
+
+/// A table schema
+typedef struct TableConfig {
+  size_t field_count;
+  size_t key_field;
+  DataType key_type;
+} TableConfig;
+
 #endif
+
